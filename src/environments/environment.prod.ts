89baeac1cd47fe5fa7885ce790b502655c8caae9
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-02-13 16:44:05 -0300

    Probnado soluciones para crud en linea

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environments = {
     production: true,
     
     Api:'https://api-portfolio-brunoporfidio.koyeb.app',
+
+    // Api: 'http://localhost:8080',
+
     isLogged: true
   };
