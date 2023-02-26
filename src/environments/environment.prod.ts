b27581dba3ff0c4c945c886c5e978a5748d7293d
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-02-26 14:55:21 -0300

    Cambiando Api a Koyeb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,10 +5,10 @@ export const environments = {
     // Api: 'http://localhost:8080',
 
     // Api en Koyeb
-    // Api:'https://api-portfolio-brunoporfidio.koyeb.app',
+    Api:'https://api-portfolio-brunoporfidio.koyeb.app',
 
     // Api Render
-    Api:'https://api-portfolio-o75z.onrender.com',
+    // Api:'https://api-portfolio-o75z.onrender.com',
 
     isLogged: true
   };
