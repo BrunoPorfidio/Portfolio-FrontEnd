c1188d26f1d375b16bc7f677ade998537ad3d97e
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-02-26 06:33:05 -0300

    Utilizando Api en Render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,14 @@
 export const environments = {
     production: true,
     
-    Api:'https://api-portfolio-brunoporfidio.koyeb.app',
-
+    // Api Remota
     // Api: 'http://localhost:8080',
+
+    // Api en Koyeb
+    // Api:'https://api-portfolio-brunoporfidio.koyeb.app',
+
+    // Api Render
+    Api:'https://api-portfolio-o75z.onrender.com',
 
     isLogged: true
   };
