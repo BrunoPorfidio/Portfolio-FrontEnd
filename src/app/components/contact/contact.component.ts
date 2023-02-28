1cfdea4de8573a807447be4cf261e72ecd8ef548
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-02-28 01:08:55 -0300

    Activando Formulario de Contacto

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -26,12 +26,6 @@ export class ContactComponent implements OnInit {
     private tokenService: TokenService,
     private personaService: PersonaService,
   ) {
-    this.contacto = new FormGroup({
-      nombre: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      asunto: new FormControl('', Validators.required),
-      mensaje: new FormControl('', [Validators.required, Validators.minLength(20)])
-    });
   }
 
   ngOnInit(): void {
