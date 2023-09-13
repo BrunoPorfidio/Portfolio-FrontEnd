bf844203125feeae3f5326a0e36319237dc830c6
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-09-13 18:24:56 -0300

    Utilizando Api Local.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,7 @@
 export const environments = {
   production: false,
 
-  Api:'https://api-portfolio-brunoporfidio.koyeb.app',
+    Api: 'http://localhost:8080',
 
-    // Api: 'http://localhost:8080',
-
-    
     isLogged: true,
   };
