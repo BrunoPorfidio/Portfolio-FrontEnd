e80dd8a865bd4502a45b537aa56a1456d04123e4
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-09-19 22:32:48 -0300

    Update Metods for create a New AboutMe.

--- src/app/modals/modal-aboutme/modal-aboutme.component.ts
+++ src/app/modals/modal-aboutme/modal-aboutme.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Persona } from 'src/app/model/Persona'; 
-import { PersonaService } from 'src/app/services/persona.service'; 
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/model/Persona';
+import { PersonaService } from 'src/app/services/persona.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal-aboutme',
@@ -10,136 +9,52 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./modal-aboutme.component.css'],
 })
 export class ModalAboutmeComponent implements OnInit {
-  @Input() title = '';
+  nombre: string;
 
-  // Variables globales
-  persona: Persona;
-  personaList: Persona[] = [];
-  isLogged: Boolean = false;
-  personaForm: FormGroup;
-  roles: string[];
-  isAdmin = false;
+  apellido: string;
 
-  public show = false;
+  subTitulo: string;
+
+  acercaMi: string;
+
+  linkedinUrl: string;
+
+  githubUrl: string;
+
+  email: string;
+
+  telefono: string;
+
+  ubicacion: string;
+
   constructor(
     private personaService: PersonaService,
-    private authService: AuthService,
-    private formBuilder: FormBuilder
-  ) {
-    this.personaForm = this.formBuilder.group({
-      id: [],
+    private router: Router
+  ) {}
 
-      nombre: ['', [Validators.required]],
-
-      apellido: ['', [Validators.required]],
-
-      subTitulo: ['', [Validators.required]],
-
-      acercaMi: ['', [Validators.required]],
-
-      urlFoto: ['', [Validators.required]],
-
-      linkedinUrl: ['', [Validators.required]],
-
-      githubUrl: ['', [Validators.required]],
-
-      telefono: ['', [Validators.required]],
-      
-      email: ['', [Validators.required]],
-      
-      ubicacion: ['', [Validators.required]],
-
-      imgBanner: ['', [Validators.required]],
-    });
-  }
-
-  ngOnInit(): void {
-    this.isLogged = this.authService.isLogged();
-
-    this.reloadDate();
-  }
-
-  /*===/ Configuraciones del formulario /===*/
-
-
-    private clearForm() {
-    this.personaForm.setValue({
-      id: '',
-
-      nombre: '',
-
-      apellido: '',
-
-      subTitulo: '',
-
-      acercaMi: '',
-
-      urlFoto: '',
-
-      linkedinUrl: '',
-
-      githubUrl: '',
-
-      telefono: '',
-
-      email: '',
-
-      ubicacion: '',
-
-      imgBanner: '',
-    });
-  }
-
-  onNewPersona() {
-    this.clearForm();
-  }
-
-  private loadForm(persona: Persona) {
-    this.personaForm.setValue({
-
-      id: persona.id,
-
-      nombre: persona.nombre,
-
-      apellido: persona.apellido,
-
-      subTitulo: persona.subTitulo,
-
-      acercaMi: persona.acercaMi,
-
-      linkedinUrl: persona.linkedinUrl,
-
-      githubUrl: persona.githubUrl,
-
-      telefono: persona.telefono,
-
-      email: persona.email,
-
-      ubicacion: persona.ubicacion,
-    });
+  ngOnInit(): void {}
+  
+  onCreate():void{
+    const persona = new Persona(
+      this.nombre,
+      this.apellido,
+      this.subTitulo,
+      this.acercaMi,
+      this.linkedinUrl,
+      this.githubUrl,
+      this.email,
+      this.telefono,
+      this.ubicacion
+       );
+      this.personaService.crearPersona(persona).subscribe(
+        (data) => {
+          this.router.navigate(['/portfolio']);
+        },
+        (err) => {
+          alert('Error al crear la Persona');
+          this.router.navigate(['/portfolio']);
+        }
+      );
   }
   
-  onSubmit() {
-    let persona: Persona = this.personaForm.value;
-    
-    (this.personaForm.get('id')?.value == '') 
-      this.personaService
-      .crearPersona(persona)
-      .subscribe((newPersona: Persona) => {
-        this.personaList.push(newPersona);
-      });
-  }
-  
-    onEditPersona(index: number) {
-      let persona: Persona = this.personaList[index];
-      this.loadForm(persona);
-    }
-  
-  // Método para recurar los datos de la base de datos
-  reloadDate() {
-    this.personaService.verPersona().subscribe((date) => {
-      this.personaList = date;
-    });
-  }
-
 }
