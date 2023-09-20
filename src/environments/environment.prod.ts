e9b07b7a4f09fa3e822159f54472650970fd9b6f
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-09-19 22:49:49 -0300

    Cambiando a DB Remota.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environments = {
     production: true,
     
     // Api Remota
-    Api: 'http://localhost:8080',
+    // Api: 'http://localhost:8080',
 
     // Api en RailWay
-    // Api:'https://portfolio-backend-production-4b6a.up.railway.app',
+    Api:'https://portfolio-backend-production-4b6a.up.railway.app',
     
     isLogged: true
   };
