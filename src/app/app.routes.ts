12b913ebfb8815f2b145032102eb9a0c78b26edc
Bruno Porfidio <brunoporfidio117@gmail.com>
2025-07-31 15:36:35 -0300

    Fixed Email Sender.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,6 @@ import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
 
-    { path: 'portfolio', component: PortfolioComponent },
-
     { path: 'home', component: HomeComponent },
-
 
 ];
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -11,8 +12,12 @@ export class ContactComponent {
   feedbackMessage: string = '';
   feedbackClass: string = '';
 
-  onSubmit(form: NgForm) {
-    if (form.invalid) {
+  @ViewChild('contactForm') contactForm!: NgForm;
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    if (this.contactForm.invalid) {
       this.feedbackMessage = 'Please fill out all fields correctly.';
       this.feedbackClass = 'text-red-500';
       return;
@@ -21,14 +26,24 @@ export class ContactComponent {
     this.feedbackMessage = 'Sending...';
     this.feedbackClass = 'text-cyan-300';
 
-    setTimeout(() => {
-      this.feedbackMessage = 'Message sent! Thank you for reaching out.';
-      this.feedbackClass = 'text-green-400';
-      form.resetForm();
+    emailjs
+      .sendForm('service_a2y758t', 'template_w9ex4zq', e.target as HTMLFormElement, {
+        publicKey: 'qTKKh7hSbBYfSsI1t',
+      })
+      .then(
+        (response: EmailJSResponseStatus) => {
+          console.log('SUCCESS!', response.status, response.text);
+          this.feedbackMessage = 'Message sent! Thank you for reaching out.';
+          this.feedbackClass = 'text-green-400';
+          this.contactForm.resetForm();
 
-      setTimeout(() => {
-        this.feedbackMessage = '';
-      }, 4000);
-    }, 1500);
+          setTimeout(() => this.feedbackMessage = '', 5000);
+        },
+        (error) => {
+          console.log('FAILED...', error);
+          this.feedbackMessage = 'Failed to send the message. Please try again.';
+          this.feedbackClass = 'text-red-500';
+        },
+      );
   }
 }
